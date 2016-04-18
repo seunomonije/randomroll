fe0991750e71869325bd1b8c913700171e9ea9f6
seunomonije <oseun22@gmail.com>
2016-04-18 08:07:58 -0500

    4.18.16
    yahh

--- RandomRoll/GameScene.swift
+++ RandomRoll/GameScene.swift
@@ -24,6 +24,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     let gameName = SKSpriteNode(imageNamed: "randomrolllogo")
     let background1 = SKSpriteNode(imageNamed: "defaultbackground")
     let background2 = SKSpriteNode(imageNamed: "defaultbackground")
+    let background3 = SKSpriteNode(imageNamed: "defaultbackground")
     let cloud = SKSpriteNode(imageNamed: "scrollingclouds")
     let monster = SKSpriteNode(imageNamed: "barrelSprite")
     let button = SKSpriteNode(imageNamed: "projectile")
@@ -90,6 +91,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
    override func update(currentTime: NSTimeInterval) {
         background1.position = CGPoint(x: background1.position.x - 1, y: background1.position.y)
         background2.position = CGPoint(x: background2.position.x - 1, y: background2.position.y)
+        background3.position = CGPoint(x: background2.position.x - 1, y: background2.position.y)
         
         if (background1.position.x < -background1.size.width){
             background1.position = CGPointMake(background2.position.x + background2.size.width, background1.position.y)
@@ -98,6 +100,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         if background2.position.x < -background2.size.width {
             background2.position = CGPointMake(background1.position.x + background1.size.width, background2.position.y)
         }
+       
     }
     func gameNameSetUp(){
         gameName.position = CGPoint(x: size.width/2, y: size.height / 1.2)
@@ -117,6 +120,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         background2.position = CGPointMake(background2.size.width - 1, 0)
         background2.size = CGSize(width: fraction , height: size.height)
         background2.zPosition = -1
+        addChild(background2)
+        
+        background3.anchorPoint = CGPointZero
+        background3.position = CGPointMake(background3.size.width - 1, 0)
+        background3.size = CGSize(width: fraction , height: size.height)
+        background3.zPosition = -1
         addChild(background2)
     }
     
